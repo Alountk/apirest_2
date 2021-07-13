d1ce5803ffa315686963a58c23d87fdac8719735
Raul Marchan Catala <alountk@gmail.com>
2021-07-13 21:20:22 +0200

    getItem and getItems

---
+++ api/Repositories/InMemItemsRepository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using api.Entities;
+
+namespace api.Repositories
+{
+  public class InMemItemsRepository
+  {
+    private readonly List<Item> items = new()
+    {
+      new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+      new Item { Id = Guid.NewGuid(), Name = "Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+      new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 8, CreatedDate = DateTimeOffset.UtcNow },
+    };
+
+    public IEnumerable<Item> GetItems()
+    {
+      return items;
+    }
+
+    public Item GetItem(Guid id)
+    {
+      return items.Where(item => item.Id == id).SingleOrDefault();
+    }
+  }
+}
