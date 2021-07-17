2b15eea95c8eb8eec8bb524938f0292f2281e652
Raul Marchan Catala <alountk@gmail.com>
2021-07-17 12:36:38 +0200

    two test workings

--- Skeleton.UnitTests/ItemsControllerTests.cs
+++ Skeleton.UnitTests/ItemsControllerTests.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Skeleton.Api.Controllers;
+using Skeleton.Api.DTOs;
 using Skeleton.Api.Entities;
 using Skeleton.Api.Repositories;
 using Xunit;
@@ -12,15 +14,16 @@ namespace Skeleton.UnitTests
 {
     public class ItemsControllerTests
     {
+        private readonly Mock<IItemsRepository> repositoryStub = new();
+        private readonly Mock<ILogger<ItemsController>> loggerStub = new();
+        private readonly Random rand = new();
+
         [Fact]
         public async Task GetItemAsync_WithUnexistingItem_ReturnsNotFound()
         {
             // Arrange
-            var repositoryStub = new Mock<IItemsRepository>();
             repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
                 .ReturnsAsync((Item)null);
-
-            var loggerStub = new Mock<ILogger<ItemsController>>();
 
             var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
 
@@ -29,6 +32,37 @@ namespace Skeleton.UnitTests
 
             // Assert
             Assert.IsType<NotFoundResult>(result.Result);
+        }
+
+        [Fact]
+        public async Task GetItemAsync_WithExistingItem_ReturnsExpectedItem()
+        {
+            // Arrange
+            var expectedItem = CreateRandomItem();
+
+            repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(expectedItem);
+
+            var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
+
+            // Act
+            var result = await controller.GetItemAsync(Guid.NewGuid());
+
+            // Assert
+            result.Value.Should().BeEquivalentTo(
+                expectedItem,
+                options => options.ComparingByMembers<Item>());
+        }
+
+        private Item CreateRandomItem()
+        {
+            return new()
+            {
+                Id = Guid.NewGuid(),
+                Name = Guid.NewGuid().ToString(),
+                Price = rand.Next(1000),
+                CreatedDate = DateTimeOffset.UtcNow
+            };
         }
     }
 }
