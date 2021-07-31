b2f40685d53b5902674f05c4acf616cc4ae3502d
Raul Marchan Catala <alountk@gmail.com>
2021-07-31 14:06:28 +0200

    add DTO article

--- Skeleton.Api/DTOs.cs
+++ Skeleton.Api/DTOs.cs
@@ -3,7 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Skeleton.Api.DTOs
 {
-    public record ItemDTO(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
-    public record CreateItemDTO([Required][MaxLength(50)] string Name, [MaxLength(300)] string Description, [Range(1, 1000)] decimal Price);
-    public record UpdateItemDTO([Required][MaxLength(50)] string Name, [MaxLength(300)] string Description, [Range(1, 1000)] decimal Price);
+  public record ItemDTO(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+  public record CreateItemDTO([Required][MaxLength(50)] string Name, [MaxLength(300)] string Description, [Range(1, 1000)] decimal Price);
+  public record UpdateItemDTO([Required][MaxLength(50)] string Name, [MaxLength(300)] string Description, [Range(1, 1000)] decimal Price);
+  public record articleDTO(Guid Id, string Title, string BodyText, DateTimeOffset CreatedDate);
+  public record CreateArticleDTO([Required][MaxLength(50)] string Title, [MaxLength(1000)] string BodyText);
+  public record UpdateArticleDTO([Required][MaxLength(50)] string Title, [MaxLength(1000)] string BodyText);
 }
